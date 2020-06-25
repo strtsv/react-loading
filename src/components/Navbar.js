9def33a715c024d8907c51310924fb14abd72d66
jsusilo <enr@bk.ru>
2020-06-26 00:25:16 +0500

    router

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { FaAlignRight } from "react-icons/fa";
+
+export default class Navbar extends Component {
+  state = {
+    isOpen: false
+  };
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <button
+              type="button"
+              className="nav-btn"
+              onClick={this.handleToggle}
+            >
+              <FaAlignRight className="nav-icon" />
+            </button>
+          </div>
+          <ul
+            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const home = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default home;
